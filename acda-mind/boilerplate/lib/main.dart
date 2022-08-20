2da409b23c8a4458967e694bf952a19223fea356
cicero <bibektimilsina000@gmail.com>
2022-08-20 22:35:03 +0545

    boilerpalted app

--- acda-mind/boilerplate/lib/main.dart
+++ acda-mind/boilerplate/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Text('print hello'),
+      home: Text('hello'),
     );
   }
 }
