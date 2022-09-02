d048f2b0053d9986cf09b023c8baf2273aeb318c
Bibek Timilsina <bibektimilsina000@gmail.com>
2022-09-02 21:17:27 +0545

    Home Component

--- acda-mind/recipe/lib/main.dart
+++ acda-mind/recipe/lib/main.dart
@@ -9,6 +9,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomeApp(),
+    );
+  }
+}
+
+class HomeApp extends StatelessWidget {
+  const HomeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('recipe'),
+      ),
+      body: const Center(
+        child: Text('Navigation started'),
+      ),
+    );
   }
 }
