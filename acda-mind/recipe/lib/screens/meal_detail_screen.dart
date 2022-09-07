7be1e1fe6ff7a792162ed8db40b7597a7d02cd26
Bibek Timilsina <bibektimilsina000@gmail.com>
2022-09-07 15:56:05 +0545

    mealdetail

--- acda-mind/recipe/lib/screens/meal_detail_screen.dart
+++ acda-mind/recipe/lib/screens/meal_detail_screen.dart
@@ -8,9 +8,33 @@ class MealDetail extends StatelessWidget {
     final argRoutes =
         ModalRoute.of(context)?.settings.arguments as Map<String, String>;
 
-    return Center(
-      child: Container(
-        child: const Text('this is detail page of slected meal'),
+    final String title = argRoutes['title'] as String;
+    final String imageUrl = argRoutes['imageUrl'] as String;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(15)),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: Image.network(imageUrl),
+            ),
+          ),
+          Container(
+            child: Text(
+              title,
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
+            ),
+          ),
+          Container(
+            child: Column(),
+          )
+        ],
       ),
     );
   }
