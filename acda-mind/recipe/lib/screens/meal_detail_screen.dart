cecb31b2337e91d087fb7ab5a720f1bf0104f931
Bibek Timilsina <bibektimilsina000@gmail.com>
2022-09-07 15:26:42 +0545

    new screen meal detail created

---
+++ acda-mind/recipe/lib/screens/meal_detail_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MealDetail extends StatelessWidget {
+  const MealDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
