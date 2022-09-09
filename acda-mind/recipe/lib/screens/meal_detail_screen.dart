4c7b4b1183fb2e23292f58e031752dfe1a7fcc28
Bibek Timilsina <bibektimilsina000@gmail.com>
2022-09-09 15:50:53 +0545

    mealdetail screen

--- acda-mind/recipe/lib/screens/meal_detail_screen.dart
+++ acda-mind/recipe/lib/screens/meal_detail_screen.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../models/dummy_data.dart';
+
 class MealDetail extends StatelessWidget {
   static const routeName = 'meal-detail';
 
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context)?.settings.arguments as String;
+    final selectedMeal = DUMMY_MEALS.firstWhere(
+      (element) => element.id == mealId,
+    );
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(mealId),
+        title: Text(selectedMeal.title),
       ),
       body: Column(
         children: [
@@ -20,10 +25,12 @@ class MealDetail extends StatelessWidget {
               borderRadius: BorderRadius.circular(15),
             ),
           ),
-          Container(
-            child: const Text(
-              'stau',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
+          SizedBox(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              selectedMeal.imageUrl,
+              fit: BoxFit.cover,
             ),
           ),
           Container(
