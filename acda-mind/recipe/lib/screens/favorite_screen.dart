2a161b2ae559c3e048c67f396c9e1505cd31a92f
Bibek Timilsina <bibektimilsina000@gmail.com>
2022-09-11 20:59:31 +0545

    bottom bar added

---
+++ acda-mind/recipe/lib/screens/favorite_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Favorite extends StatelessWidget {
+  const Favorite({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('data'),
+    );
+  }
+}
