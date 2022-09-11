54c5fe8690964eb7623c68ad50f05228c88b3f8e
Bibek Timilsina <bibektimilsina000@gmail.com>
2022-09-11 20:59:19 +0545

    added botton nav bar

---
+++ acda-mind/recipe/lib/screens/tab_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:recipe/screens/category_screen.dart';
+import 'package:recipe/screens/favorite_screen.dart';
+
+class Tabs extends StatefulWidget {
+  const Tabs({super.key});
+
+  @override
+  State<Tabs> createState() => _TabsState();
+}
+
+class _TabsState extends State<Tabs> {
+  final List<Widget> _pages = [
+    const Category(),
+    const Favorite(),
+  ];
+
+  int _selectPageIndex = 0;
+
+  void _selectPage(index) {
+    setState(() {
+      _selectPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Recipe'),
+      ),
+      body: _pages[_selectPageIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        selectedItemColor: Theme.of(context).colorScheme.secondary,
+        unselectedItemColor: Colors.white,
+        onTap: _selectPage,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Category',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favorite',
+          ),
+        ],
+      ),
+    );
+  }
+}
