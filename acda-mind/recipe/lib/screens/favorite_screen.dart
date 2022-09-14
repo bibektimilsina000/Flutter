c7d2ea369ae847a7a8ea1939903e60e2062c993c
Bibek Timilsina <bibektimilsina000@gmail.com>
2022-09-14 19:36:02 +0545

    commit

--- acda-mind/recipe/lib/screens/favorite_screen.dart
+++ acda-mind/recipe/lib/screens/favorite_screen.dart
@@ -1,31 +1,25 @@
 import 'package:flutter/material.dart';
 
-import '../models/meal.dart';
 import '../widgets/meal_item.dart';
 
 class Favorite extends StatelessWidget {
-  List<Meal> favMeal;
+  List favMeal;
   Favorite(this.favMeal);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Favorite'),
-      ),
-      body: ListView.builder(
-        itemCount: favMeal.length,
-        itemBuilder: (BuildContext context, int index) {
-          return MealItem(
-            id: favMeal[index].id,
-            title: favMeal[index].title,
-            imageUrl: favMeal[index].imageUrl,
-            duration: favMeal[index].duration,
-            affordability: favMeal[index].affordability,
-            complexity: favMeal[index].complexity,
-          );
-        },
-      ),
+    return ListView.builder(
+      itemCount: favMeal.length,
+      itemBuilder: (BuildContext context, int index) {
+        return MealItem(
+          id: favMeal[index].id,
+          title: favMeal[index].title,
+          imageUrl: favMeal[index].imageUrl,
+          duration: favMeal[index].duration,
+          affordability: favMeal[index].affordability,
+          complexity: favMeal[index].complexity,
+        );
+      },
     );
   }
 }
