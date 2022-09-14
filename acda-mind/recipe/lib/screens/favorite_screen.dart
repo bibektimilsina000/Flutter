c86926a9c49afa1cf30abf8474c802cf030100f5
Bibek Timilsina <bibektimilsina000@gmail.com>
2022-09-14 19:20:50 +0545

    added fav time to run

--- acda-mind/recipe/lib/screens/favorite_screen.dart
+++ acda-mind/recipe/lib/screens/favorite_screen.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
 
+import '../models/meal.dart';
+import '../widgets/meal_item.dart';
+
 class Favorite extends StatelessWidget {
-  List favMeal;
+  List<Meal> favMeal;
   Favorite(this.favMeal);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('your fav'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Favorite'),
+      ),
+      body: ListView.builder(
+        itemCount: favMeal.length,
+        itemBuilder: (BuildContext context, int index) {
+          return MealItem(
+            id: favMeal[index].id,
+            title: favMeal[index].title,
+            imageUrl: favMeal[index].imageUrl,
+            duration: favMeal[index].duration,
+            affordability: favMeal[index].affordability,
+            complexity: favMeal[index].complexity,
+          );
+        },
+      ),
     );
   }
 }
