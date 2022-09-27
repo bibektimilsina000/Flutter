4fc4928e05b96e7dbdec061e877b2654262b3c94
Bibek Timilsina <bibektimilsina000@gmail.com>
2022-09-27 20:11:01 +0545

    detail

--- acda-mind/epasal/lib/screens/detail_product.dart
+++ acda-mind/epasal/lib/screens/detail_product.dart
@@ -5,16 +5,4 @@ class ProductDetail extends StatelessWidget {
 
   const ProductDetail({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    final id = ModalRoute.of(context)?.settings.arguments as String;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(id),
-      ),
-      body: const Center(
-        child: Text('Hello World'),
-      ),
-    );
-  }
-}
+ 
