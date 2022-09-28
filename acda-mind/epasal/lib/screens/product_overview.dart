ca02dcc6dbc34b7d55b086f97484d1d88fa7b368
Bibek Timilsina <bibektimilsina000@gmail.com>
2022-09-28 13:11:29 +0545

    recfatoring

--- acda-mind/epasal/lib/screens/product_overview.dart
+++ acda-mind/epasal/lib/screens/product_overview.dart
@@ -48,30 +48,44 @@ class ProductOverview extends StatelessWidget {
       appBar: AppBar(
         title: const Text('ePasal'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 4,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        itemCount: lodedProducts.length,
-        itemBuilder: (BuildContext context, int index) {
-          return GestureDetector(
-            onTap: () {
-              Navigator.of(context).pushNamed(ProductDetail.routeName,
-                  arguments: lodedProducts[index].id);
-            },
-            child: ProductItem(
-                id: lodedProducts[index].id,
-                title: lodedProducts[index].title,
-                description: lodedProducts[index].description,
-                price: lodedProducts[index].price,
-                imageUrl: lodedProducts[index].imageUrl),
-          );
-        },
+      body: ProductGridView(lodedProducts: lodedProducts),
+    );
+  }
+}
+
+class ProductGridView extends StatelessWidget {
+  const ProductGridView({
+    Key? key,
+    required this.lodedProducts,
+  }) : super(key: key);
+
+  final List<Product> lodedProducts;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 4,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
       ),
+      itemCount: lodedProducts.length,
+      itemBuilder: (BuildContext context, int index) {
+        return GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(ProductDetail.routeName,
+                arguments: lodedProducts[index].id);
+          },
+          child: ProductItem(
+              id: lodedProducts[index].id,
+              title: lodedProducts[index].title,
+              description: lodedProducts[index].description,
+              price: lodedProducts[index].price,
+              imageUrl: lodedProducts[index].imageUrl),
+        );
+      },
     );
   }
 }
