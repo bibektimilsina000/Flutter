faa3457deb82e362e1c4a197059008555afb4b4a
Bibek Timilsina <bibektimilsina000@gmail.com>
2022-10-01 19:58:02 +0545

    statemanagement

---
+++ acda-mind/epasal/lib/providers/product.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/foundation.dart';
+
+class Product with ChangeNotifier {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool isFav;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+    this.isFav = false,
+  });
+
+  void toggleFavoriteStatus() {
+    isFav = !isFav;
+    notifyListeners();
+  }
+}
