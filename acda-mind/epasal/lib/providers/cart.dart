ef0591fed9407c32b87ac84ea0d6ecfc204618a5
Bibek Timilsina <bibektimilsina000@gmail.com>
2022-10-04 11:42:04 +0545

    card added

---
+++ acda-mind/epasal/lib/providers/cart.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem({
+    required this.id,
+    required this.title,
+    required this.quantity,
+    required this.price,
+  });
+}
+
+class Cart with ChangeNotifier {
+  late Map<String, CartItem> _item;
+
+  Map<String, CartItem> get item {
+    return {..._item};
+  }
+
+  void addItem(String productId, String title, double price) {
+    if (_item.containsKey(productId)) {
+      _item.update(
+        productId,
+        (value) => CartItem(
+          id: value.id,
+          title: value.title,
+          quantity: value.quantity + 1,
+          price: value.price,
+        ),
+      );
+    } else {
+      _item.putIfAbsent(
+        productId,
+        () => CartItem(
+          id: DateTime.now().toString(),
+          title: title,
+          quantity: 1,
+          price: price,
+        ),
+      );
+    }
+  }
+}
