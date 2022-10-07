cd139ba5b56ac9849ab21156e59dc0627c4699ff
Bibek Timilsina <bibektimilsina000@gmail.com>
2022-10-07 20:20:11 +0545

    edit_product_page_added

---
+++ acda-mind/epasal/lib/screens/edit_product_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class EditProductScreen extends StatefulWidget {
+  static const routeName = '/edit-product';
+  const EditProductScreen({super.key});
+
+  @override
+  State<EditProductScreen> createState() => _EditProductScreenState();
+}
+
+class _EditProductScreenState extends State<EditProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Product'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(label: Text('Title')),
+                textInputAction: TextInputAction.next,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
