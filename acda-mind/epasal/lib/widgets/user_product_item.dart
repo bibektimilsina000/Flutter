bd6df3ac46cc54a0c484136a3536c324d4d70568
Bibek Timilsina <bibektimilsina000@gmail.com>
2022-10-10 16:10:39 +0545

    delete Product

--- acda-mind/epasal/lib/widgets/user_product_item.dart
+++ acda-mind/epasal/lib/widgets/user_product_item.dart
@@ -1,9 +1,13 @@
+import 'package:epasal/providers/products.dart';
+import 'package:epasal/screens/edit_product_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class UserProductItem extends StatelessWidget {
+  final String id;
   final String title;
   final String imageUrl;
-  const UserProductItem(this.title, this.imageUrl, {super.key});
+  const UserProductItem(this.id, this.title, this.imageUrl, {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,13 +22,18 @@ class UserProductItem extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.of(context)
+                      .pushNamed(EditProductScreen.routeName, arguments: id);
+                },
                 icon: Icon(
                   Icons.edit,
                   color: Theme.of(context).colorScheme.primary,
                 )),
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                Provider.of<Products>(context, listen: false).deleteProduct(id);
+              },
               icon: const Icon(Icons.delete, color: Colors.red),
             )
           ],
