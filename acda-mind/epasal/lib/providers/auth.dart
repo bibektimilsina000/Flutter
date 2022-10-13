333fdb66e57ede20e7569502213615b22fd0207c
Bibek Timilsina <bibektimilsina000@gmail.com>
2022-10-13 11:50:31 +0545

    error dialog when login failed

--- acda-mind/epasal/lib/providers/auth.dart
+++ acda-mind/epasal/lib/providers/auth.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:epasal/models/http_exception.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -12,14 +13,24 @@ class Auth with ChangeNotifier {
       String email, String password, String urlSection) async {
     final url =
         'https://identitytoolkit.googleapis.com/v1/accounts:$urlSection?key=AIzaSyC8yczbfGVTfKrSC1wjwkSnHmzqbHsGkiA';
-
-    final response = await http.post(Uri.parse(url),
-        body: json.encode({
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        }));
-    print(json.decode(response.body));
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      var responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
   Future<void> signUp(email, password) async {
