a71873b16f4bf2a22f3bf8ab909370eb9204da8a
Bibek Timilsina <bibektimilsina000@gmail.com>
2022-10-13 21:01:40 +0545

    user specefic manage product

--- acda-mind/epasal/lib/providers/products.dart
+++ acda-mind/epasal/lib/providers/products.dart
@@ -40,6 +40,10 @@ class Products with ChangeNotifier {
     //       'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
     // ),
   ];
+  final String authToken;
+  String userId;
+
+  Products(this.authToken, this.userId, this._items);
 
   List<Product> get items {
     return [..._items];
@@ -63,29 +67,42 @@ class Products with ChangeNotifier {
     return _items.firstWhere((element) => element.id == id);
   }
 
-  Future<void> fetchProduct() async {
-    final url = 'https://epasal-edd08-default-rtdb.firebaseio.com/product.json';
+  Future<void> fetchProduct([bool filterByUser = false]) async {
+    var filterString = filterByUser ? 'orderBy="userId"&equalTo="$userId"' : '';
+    var url =
+        'https://epasal-edd08-default-rtdb.firebaseio.com/product.json?auth=$authToken&$filterString';
     try {
       final response = await http.get(Uri.parse(url));
 
       final exatractedData = json.decode(response.body) as Map<String, dynamic>;
+      print(exatractedData);
       if (exatractedData == null) {
         return;
       }
+
+      url =
+          'https://epasal-edd08-default-rtdb.firebaseio.com/userFavorite/$userId.json?auth=$authToken';
+      final favResponse = await http.get(Uri.parse(url));
+
+      final favData = json.decode(favResponse.body);
+
       final List<Product> lodedProduct = [];
 
       exatractedData.forEach(
         (prodId, prodData) {
           lodedProduct.add(Product(
-              id: prodId,
-              title: prodData['title'],
-              description: prodData['description'],
-              price: prodData['price'],
-              imageUrl: prodData['imageUrl'],
-              isFav: prodData['isFav']));
+            id: prodId,
+            title: prodData['title'],
+            description: prodData['description'],
+            price: prodData['price'],
+            imageUrl: prodData['imageUrl'],
+            isFav: favData == null ? false : favData[prodId] ?? false,
+          ));
         },
       );
+
       _items = lodedProduct;
+
       notifyListeners();
     } catch (error) {
       print(error);
@@ -94,7 +111,8 @@ class Products with ChangeNotifier {
   }
 
   Future<void> addProduct(Product product) async {
-    final url = 'https://epasal-edd08-default-rtdb.firebaseio.com/product.json';
+    final url =
+        'https://epasal-edd08-default-rtdb.firebaseio.com/product.json?auth=$authToken';
 
     try {
       var response = await http.post(Uri.parse(url),
@@ -103,16 +121,16 @@ class Products with ChangeNotifier {
             'description': product.description,
             'price': product.price,
             'imageUrl': product.imageUrl,
-            'isFav': product.isFav,
+            'userId': userId,
           }));
 
       final newProduct = Product(
-          id: json.decode(response.body)['name'],
-          title: product.title,
-          description: product.description,
-          price: product.price,
-          imageUrl: product.imageUrl,
-          isFav: product.isFav);
+        id: json.decode(response.body)['name'],
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        imageUrl: product.imageUrl,
+      );
 
       _items.add(newProduct);
       notifyListeners();
@@ -126,7 +144,7 @@ class Products with ChangeNotifier {
 
     try {
       final url =
-          'https://epasal-edd08-default-rtdb.firebaseio.com/product/${id}.json';
+          'https://epasal-edd08-default-rtdb.firebaseio.com/product/${id}.json?auth=$authToken';
 
       await http.patch(Uri.parse(url),
           body: json.encode({
@@ -144,7 +162,7 @@ class Products with ChangeNotifier {
 
   Future<void> deleteProduct(String id) async {
     final url =
-        'https://epasal-edd08-default-rtdb.firebaseio.com/product/${id}.json';
+        'https://epasal-edd08-default-rtdb.firebaseio.com/product/${id}.json?auth=$authToken';
 
     final response = await http.delete(Uri.parse(url));
     if (response.statusCode >= 400) {
