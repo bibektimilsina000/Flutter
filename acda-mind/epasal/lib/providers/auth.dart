4884a527100dfc054ffc4e8b3c49e8edc97f4116
Bibek Timilsina <bibektimilsina000@gmail.com>
2022-10-15 00:20:16 +0545

    auto login

--- acda-mind/epasal/lib/providers/auth.dart
+++ acda-mind/epasal/lib/providers/auth.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:epasal/models/http_exception.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Auth with ChangeNotifier {
   String _token = '';
@@ -48,6 +49,13 @@ class Auth with ChangeNotifier {
           .add(Duration(seconds: int.parse(responseData['expiresIn'])));
       _userId = responseData['localId'];
       notifyListeners();
+      final pref = await SharedPreferences.getInstance();
+      final userData = json.encode({
+        'token': _token,
+        'userId': _userId,
+        'expiryDate': _experyDate.toIso8601String()
+      });
+      pref.setString('userData', userData);
     } catch (error) {
       throw error;
     }
@@ -59,6 +67,27 @@ class Auth with ChangeNotifier {
 
   Future<void> signIn(email, password) async {
     return _authenticate(email, password, 'signInWithPassword');
+  }
+
+  Future<bool> tryAutoLogin() async {
+    final pref = await SharedPreferences.getInstance();
+    if (!pref.containsKey('userData')) {
+      return false;
+    }
+    final getExatractedData = json.decode(pref.getString('userData').toString())
+        as Map<String, Object>;
+    final exatractedDate =
+        DateTime.parse(getExatractedData['expiryDate'].toString());
+
+    if (exatractedDate.isBefore(DateTime.now())) {
+      return false;
+    }
+
+    _token = getExatractedData['token'].toString();
+    _userId = getExatractedData['expiryDate'].toString();
+    _experyDate = exatractedDate;
+    notifyListeners();
+    return true;
   }
 
   void signOut() {
