0f441acdfd6b5841a9510c4a5b478536502def8f
bibektimilsina000 <bibektimilsina000@gmail.com>
2022-11-03 14:22:37 +0545

    started filebase

---
+++ acda-mind/wechat/lib/screens/chat_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              child: const Text('working'),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
